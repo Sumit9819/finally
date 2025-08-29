bf334a4c635f3722e660ed903a75cd06fffba310
Sumit9819 <44696990+Sumit9819@users.noreply.github.com>
2025-08-29 22:27:54 +0545

    Update [video_id].js

--- api/[video_id].js
+++ api/[video_id].js
@@ -4,18 +4,25 @@ import path from 'path';
 const videoData = {
     "10035": {
         "redirect_url": "https://otieu.com/4/9786651",
-        "video_path": "https://finally-eta-seven.vercel.app/videos/10035.mp4",
+        "video_path": "https://osmana.vercel.app/videos/10035.mp4",
         "title": "Backshot 10035",
         "description": "Get the 10035 backshot.",
-        "thumbnail": "https://finally-eta-seven.vercel.app/videos/10035.jpg", // New field for the thumbnail URL
+        "thumbnail": "https://osmana.vercel.app/videos/10035.jpg",
     },
     "10036": {
-        "redirect_url": "https://otieu.com/4/9786651",
-        "video_path": "https://finally-eta-seven.vercel.app/videos/10036.mp4",
-        "title": "Backshot 10036",
-        "description": "Get the 10036 backshot.",
-        "thumbnail": "https://finally-eta-seven.vercel.app/videos/10036.jpg",
+        "redirect_url": "https://otieu.com/5/1234567",
+        "video_path": "https://osmana.vercel.app/videos/10036.mp4",
+        "title": "Example Video 10036",
+        "description": "This is a description for video 10036.",
+        "thumbnail": "https://osmana.vercel.app/videos/10036.jpg",
     },
+    "10037": {
+        "redirect_url": "https://otieu.com/6/7654321",
+        "video_path": "https://osmana.vercel.app/videos/10037.mp4",
+        "title": "Next Video 10037",
+        "description": "A new video just for you.",
+        "thumbnail": "https://osmana.vercel.app/videos/10037.jpg",
+    }
 };
 
 export default async function handler(req, res) {
@@ -25,27 +32,62 @@ export default async function handler(req, res) {
     }
 
     const data = videoData[video_id];
-    const playerUrl = `https://${req.headers.host}/player/${video_id}`;
-    const redirectUrl = data.redirect_url;
+    const isPlayerRequest = req.query.player === 'true';
+
+    // If the request is for the video player itself
+    if (isPlayerRequest) {
+        const playerContent = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>Video Player</title>
+                <style>
+                    body, html {
+                        margin: 0;
+                        padding: 0;
+                        width: 100%;
+                        height: 100%;
+                        overflow: hidden;
+                    }
+                    video {
+                        width: 100%;
+                        height: 100%;
+                        object-fit: contain;
+                    }
+                </style>
+            </head>
+            <body>
+                <video controls autoplay>
+                    <source src="${data.video_path}" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </body>
+            </html>
+        `;
+        res.setHeader('Content-Type', 'text/html');
+        return res.status(200).send(playerContent);
+    }
     
+    // This is the initial page with the Twitter Card metadata
     const htmlContent = `
         <!DOCTYPE html>
         <html>
         <head>
             <meta charset="UTF-8">
-            <meta http-equiv="refresh" content="0; url=${redirectUrl}">
+            <meta http-equiv="refresh" content="0; url=${data.redirect_url}">
             <title>${data.title}</title>
             <meta name="twitter:card" content="player">
             <meta name="twitter:site" content="@your_twitter_handle">
             <meta name="twitter:title" content="${data.title}">
             <meta name="twitter:description" content="${data.description}">
-            <meta name="twitter:player" content="${playerUrl}">
+            <meta name="twitter:player" content="https://${req.headers.host}/${video_id}?player=true">
             <meta name="twitter:player:width" content="1280">
             <meta name="twitter:player:height" content="720">
             <meta name="twitter:image" content="${data.thumbnail}">
         </head>
         <body>
-            <p>If you are not redirected, please <a href="${redirectUrl}">click here</a>.</p>
+            <p>If you are not redirected, please <a href="${data.redirect_url}">click here</a>.</p>
         </body>
         </html>
     `;
