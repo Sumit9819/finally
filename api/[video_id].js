80cc9f668e2f0276960fe9354732e618cafce87a
Sumit9819 <44696990+Sumit9819@users.noreply.github.com>
2025-08-29 22:48:58 +0545

    Update [video_id].js

--- api/[video_id].js
+++ api/[video_id].js
@@ -1,40 +1,26 @@
-import fs from 'fs';
-import path from 'path';
-
+// A simple object to hold your video data.
 const videoData = {
-    "10035": {
-        "redirect_url": "https://otieu.com/4/9786651",
-        "video_path": "https://finally-eta-seven.vercel.app/videos/10035.mp4",
-        "title": "Backshot 10035",
-        "description": "Get the 10035 backshot.",
-        "thumbnail": "https://finally-eta-seven.vercel.app/videos/10035.jpg",
-    },
     "10036": {
         "redirect_url": "https://otieu.com/5/1234567",
         "video_path": "https://finally-eta-seven.vercel.app/videos/10036.mp4",
-        "title": "Example Video 10036",
+        "title": "Love this shot",
         "description": "This is a description for video 10036.",
-        "thumbnail": "https://finally-eta-seven.vercel.app/videos/10036.jpg",
-    },
-    "10037": {
-        "redirect_url": "https://otieu.com/6/7654321",
-        "video_path": "https://finally-eta-seven.vercel.app/videos/10037.mp4",
-        "title": "Next Video 10037",
-        "description": "A new video just for you.",
-        "thumbnail": "https://finally-eta-seven.vercel.app/videos/10037.jpg",
+        "thumbnail": "https://finally-eta-seven.vercel.app/videos/10036.jpg"
     }
 };
 
 export default async function handler(req, res) {
-    const { video_id } = req.query;
-    if (!video_id || !videoData[video_id]) {
+    const { videoId } = req.query;
+
+    // Check if the video ID exists in our data.
+    if (!videoId || !videoData[videoId]) {
         return res.status(404).send("Video not found.");
     }
 
-    const data = videoData[video_id];
+    const data = videoData[videoId];
     const isPlayerRequest = req.query.player === 'true';
 
-    // If the request is for the video player itself
+    // If the request is from Twitter's Player Card, serve the video player HTML.
     if (isPlayerRequest) {
         const playerContent = `
             <!DOCTYPE html>
@@ -69,7 +55,7 @@ export default async function handler(req, res) {
         return res.status(200).send(playerContent);
     }
     
-    // This is the initial page with the Twitter Card metadata
+    // Otherwise, serve the initial HTML with the Twitter Card metadata.
     const htmlContent = `
         <!DOCTYPE html>
         <html>
@@ -81,7 +67,7 @@ export default async function handler(req, res) {
             <meta name="twitter:site" content="@your_twitter_handle">
             <meta name="twitter:title" content="${data.title}">
             <meta name="twitter:description" content="${data.description}">
-            <meta name="twitter:player" content="https://${req.headers.host}/${video_id}?player=true">
+            <meta name="twitter:player" content="https://${req.headers.host}/${videoId}?player=true">
             <meta name="twitter:player:width" content="1280">
             <meta name="twitter:player:height" content="720">
             <meta name="twitter:image" content="${data.thumbnail}">
